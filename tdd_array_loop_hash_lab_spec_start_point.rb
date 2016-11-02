2ed77262f919adc7df919ba55810b4a5e685ce0e
Winnie Ho <winnie-ho@hotmail.co.uk>
2016-11-02 17:47:29 +0000

    backing up lab

---
+++ tdd_array_loop_hash_lab_spec_start_point.rb
@@ -0,0 +1,36 @@
+require('minitest/autorun')
+require_relative('./tdd_array_loop_hash_lab_start_point')
+
+class Lab < MiniTest::Test
+
+  def test_add_array_lengths
+result = add_array_lengths
+assert_equal(8, result)
+  end
+
+
+  def test_sum_array
+sum_result = sum_array
+assert_equal(15,sum_result)
+  end
+
+
+  def test_find_item
+find_result = find_item?("Ravenclaw")
+assert_equal(true, find_result)
+  end
+
+
+  def test_first_key_name
+    result = first_key_name
+    assert_equal("Sandy",result)
+
+  end
+
+
+def test_array_of_capitals
+result = array_of_capitals()
+assert_equal('London', 'Paris', 'Rome',result)
+  end
+
+end
---
+++ tdd_array_loop_hash_lab_start_point.rb
@@ -0,0 +1,61 @@
+def add_array_lengths
+  prices = [ 1.23, 6.98, 8.43, 2.45 ]
+  costs = [ 4.23, 1.12, 0.52, 8.67 ]
+
+  return prices.length + costs.length
+end
+
+def sum_array()
+data = [1, 2, 3, 4, 5]
+
+ total = 0
+  for num in data
+    total += num
+  end
+
+return total
+end
+
+def find_item? (house)
+  hogwarts = [ 'Hufflepuff', 'Slytherin', 'Gryffindor', 'Ravenclaw' ]
+
+    for team in hogwarts
+      return true if house == team
+    end
+    return false
+end
+
+def first_key_name
+  teacher_wallets = {
+    'Sandy' => 12,
+    'Zsolt'  => 10,
+    'Val'  => 1356,
+    'Jay' => 1
+  }
+
+  for key, value in teacher_wallets 
+    return key
+  end
+end
+
+def array_of_capitals(info)
+  countries = {
+    uk: {
+      capital: 'London',
+      population: 60
+    },
+    france: {
+      capital: 'Paris',
+      population: 70
+    },
+    italy: {
+      capital: 'Rome',
+      population: 56
+    }
+  }
+
+return array_of_capitals(countries[:capital])
+
+
+
+end
